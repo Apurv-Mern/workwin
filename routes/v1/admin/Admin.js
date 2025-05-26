75406629c82e7cf96698ce4e397f86abd74e51f5
Apurv Gupta <apurv.gupta@dotsquares.com>
2025-05-26 18:04:32 +0530

    admin

--- routes/v1/admin/Admin.js
+++ routes/v1/admin/Admin.js
@@ -4,7 +4,8 @@ const config = require("config");
 const { sequelize } = require("../../../models");
 const initModels = require("../../../models/init-models");
 const ModelsData = initModels(sequelize);
-const { Users, Session, Roles, Permissions, UserRoles } = ModelsData;
+const { Users, Session, Roles, Permissions, UserRoles, RolePermissions } =
+  ModelsData;
 const HelperUtils = require("./../../../utils/helpers");
 // const HelperOpenAi = require("./../../../utils/openAiHelper");
 const jwt = require("jsonwebtoken");
@@ -193,7 +194,7 @@ router.get("/me", adminAuthMiddleware, async (req, res) => {
 router.get(
   "/roles",
   adminAuthMiddleware,
-  checkPermission("role.view"),
+  // checkPermission("role.view"),
   async (req, res) => {
     try {
       const userRoles = req.user.roles || []; // case-sensitive, as-is
@@ -246,66 +247,66 @@ router.get(
 );
 
 // POST /admin/roles/create
-router.post(
-  "/roles/create",
-  adminAuthMiddleware,
-  checkPermission("role.create"),
-  async (req, res) => {
-    let { name, description } = req.body;
+// router.post(
+//   "/roles/create",
+//   adminAuthMiddleware,
+//   checkPermission("role.create"),
+//   async (req, res) => {
+//     let { name, description } = req.body;
 
-    if (!name) {
-      return res
-        .status(400)
-        .send(HelperUtils.errorObj("Role Name is required"));
-    }
+//     if (!name) {
+//       return res
+//         .status(400)
+//         .send(HelperUtils.errorObj("Role Name is required"));
+//     }
 
-    if (!description) {
-      description = `this role is for ${name}`;
-    }
+//     if (!description) {
+//       description = `this role is for ${name}`;
+//     }
 
-    try {
-      // Create the role
-      const Userrole = req.user.roles;
-      if (!Userrole.includes("SuperAdmin")) {
-        return res
-          .status(401)
-          .send(
-            HelperUtils.errorObj(
-              "Access denied. only super admin can create this data."
-            )
-          );
-      }
-      const role = await Roles.create({ name, description });
+//     try {
+//       // Create the role
+//       const Userrole = req.user.roles;
+//       if (!Userrole.includes("SuperAdmin")) {
+//         return res
+//           .status(401)
+//           .send(
+//             HelperUtils.errorObj(
+//               "Access denied. only super admin can create this data."
+//             )
+//           );
+//       }
+//       const role = await Roles.create({ name, description });
 
-      // Assign permissions
-      // await role.setPermissions(permissionIds);
+//       // Assign permissions
+//       // await role.setPermissions(permissionIds);
 
-      // Fetch the role with assigned permissions
-      const fullRole = await Roles.findByPk(role.id, {
-        // include: {
-        //   model: Permissions,
-        //   as: 'Permissions',
-        //   through: { attributes: [] }
-        // }
-      });
+//       // Fetch the role with assigned permissions
+//       const fullRole = await Roles.findByPk(role.id, {
+//         // include: {
+//         //   model: Permissions,
+//         //   as: 'Permissions',
+//         //   through: { attributes: [] }
+//         // }
+//       });
 
-      const formatted = {
-        id: fullRole.id,
-        name: fullRole.name,
-        description: fullRole.description,
-        permissions: [],
-        // permissions: fullRole.Permissions.map(p => ({ id: p.id, name: p.name }))
-      };
+//       const formatted = {
+//         id: fullRole.id,
+//         name: fullRole.name,
+//         description: fullRole.description,
+//         permissions: [],
+//         // permissions: fullRole.Permissions.map(p => ({ id: p.id, name: p.name }))
+//       };
 
-      res
-        .status(200)
-        .send(HelperUtils.successObj("Role created successfully", formatted));
-    } catch (err) {
-      console.error("Error creating role:", err);
-      res.status(500).send(HelperUtils.errorObj("Failed to create role"));
-    }
-  }
-);
+//       res
+//         .status(200)
+//         .send(HelperUtils.successObj("Role created successfully", formatted));
+//     } catch (err) {
+//       console.error("Error creating role:", err);
+//       res.status(500).send(HelperUtils.errorObj("Failed to create role"));
+//     }
+//   }
+// );
 
 // PUT /admin/roles/:id
 router.put(
@@ -314,7 +315,7 @@ router.put(
   checkPermission("role.edit"),
   async (req, res) => {
     const { id } = req.params;
-    let { name, description } = req.body;
+    let { name, description, permissions } = req.body;
 
     if (!name) {
       return res
@@ -323,7 +324,16 @@ router.put(
     }
 
     if (!description) {
-      description = `this role is for ${name}`;
+      description = `This role is for ${name}`;
+    }
+
+    if (
+      permissions &&
+      (!Array.isArray(permissions) || permissions.length === 0)
+    ) {
+      return res
+        .status(400)
+        .send(HelperUtils.errorObj("Permissions must be a non-empty array"));
     }
 
     try {
@@ -333,21 +343,46 @@ router.put(
           .status(401)
           .send(
             HelperUtils.errorObj(
-              "Access denied. only super admin can edit this data."
+              "Access denied. Only SuperAdmin can edit roles."
             )
           );
       }
+
       const role = await Roles.findByPk(id);
       if (!role) {
-        return res.status(400).send(HelperUtils.errorObj("Role not found"));
+        return res.status(404).send(HelperUtils.errorObj("Role not found"));
       }
 
       await role.update({ name, description });
 
+      if (permissions) {
+        await RolePermissions.destroy({ where: { roleId: id } });
+
+        const rolePermissionsData = permissions.map((permissionId) => ({
+          roleId: id,
+          permissionId,
+        }));
+
+        await RolePermissions.bulkCreate(rolePermissionsData);
+      }
+
+      // Fetch the updated role with permissions
+      const updatedRole = await Roles.findByPk(id, {
+        include: {
+          model: Permissions,
+          as: "Permissions",
+          through: { attributes: [] },
+        },
+      });
+
       const formatted = {
-        id: role.id,
-        name: role.name,
-        description: role.description,
+        id: updatedRole.id,
+        name: updatedRole.name,
+        description: updatedRole.description,
+        permissions: updatedRole.Permissions.map((p) => ({
+          id: p.id,
+          name: p.name,
+        })),
       };
 
       res
@@ -762,6 +797,80 @@ router.post("/xp/calculate-daily", adminAuthMiddleware, async (req, res) => {
   }
 });
 
+// Create a new user
+router.post("/users/create", adminAuthMiddleware, async (req, res) => {
+  try {
+    const { firstname, lastname, email, roleId, password, userCode } = req.body;
+    if (!firstname || !lastname || !email || !roleId || !password) {
+      return res
+        .status(400)
+        .send(
+          HelperUtils.errorObj(
+            "First name, last name, email, role, and password are required"
+          )
+        );
+    }
+
+    const existingUser = await Users.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).send(HelperUtils.errorObj("User already exists"));
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const name = `${firstname} ${lastname}`;
+    const roleData = await Roles.findOne({ where: { id: roleId } });
+    if (!roleData) {
+      return res.status(400).send(HelperUtils.errorObj("Role not found"));
+    }
+    let randomEmployerCode;
+
+    if (parseInt(roleId) === 3 && userCode === "") {
+      let isUnique = false;
+
+      while (!isUnique) {
+        const randomFourDigits = Math.floor(1000 + Math.random() * 9000);
+        randomEmployerCode = `EMP${randomFourDigits}`;
+
+        const existingEmployer = await Users.findOne({
+          where: { employerCode: randomEmployerCode },
+        });
+
+        if (!existingEmployer) {
+          isUnique = true;
+        }
+      }
+    }
+
+    const user = await Users.create({
+      name,
+      email,
+      status: "active",
+      password: hashedPassword,
+      userCode: userCode || null,
+      employerCode: randomEmployerCode || null,
+    });
+    await UserRoles.create({
+      userId: user.id,
+      roleId: roleData.id,
+    });
+    // // Assign default permissions to the user based on their role
+    // const permissions = await roleData.getPermissions();
+    // for (const permission of permissions) {
+    //   await sequelize.models.UserPermissions.create({
+    //     userId: user.id,
+    //     permissionId: permission.id,
+    //   });
+    // }
+
+    res
+      .status(201)
+      .send(HelperUtils.successObj("User created successfully", user));
+  } catch (err) {
+    console.error("Error creating user:", err);
+    res.status(500).send(HelperUtils.errorObj("Failed to create user"));
+  }
+});
+
 // Get Users and roles with there permissions.
 router.get("/all-users", adminAuthMiddleware, async (req, res) => {
   try {
@@ -805,6 +914,148 @@ router.get("/all-users", adminAuthMiddleware, async (req, res) => {
   } catch (err) {
     console.error("Error fetching users:", err);
     res.status(500).send(HelperUtils.errorObj("Failed to fetch users"));
+  }
+});
+
+// Edit user
+router.put("/users/:id", adminAuthMiddleware, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, status, roleId } = req.body;
+
+    const user = await Users.findByPk(id);
+    if (!user) {
+      return res.status(404).send(HelperUtils.errorObj("User not found"));
+    }
+    await user.update({
+      name,
+      email,
+      status,
+    });
+
+    // Update user role if provided
+    if (roleId) {
+      const role = await Roles.findByPk(roleId);
+      if (!role) {
+        return res.status(400).send(HelperUtils.errorObj("Role not found"));
+      }
+
+      // Assign new role
+      await UserRoles.update(
+        {
+          userId: user.id,
+          roleId: role.id,
+        },
+        { where: { userId: user.id } }
+      );
+    }
+
+    res
+      .status(200)
+      .send(HelperUtils.successObj("User updated successfully", user));
+  } catch (err) {
+    console.error("Error updating user:", err);
+    res.status(500).send(HelperUtils.errorObj("Failed to update user"));
+  }
+});
+
+// Soft Delete user
+router.delete("/users/:id", adminAuthMiddleware, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await Users.findByPk(id);
+    if (!user) {
+      return res.status(404).send(HelperUtils.errorObj("User not found"));
+    }
+
+    await user.update({ isDeleted: 1 });
+
+    res
+      .status(200)
+      .send(HelperUtils.successObj("User deleted successfully", user));
+  } catch (err) {
+    console.error("Error deleting user:", err);
+    res.status(500).send(HelperUtils.errorObj("Failed to delete user"));
+  }
+});
+
+// Create a new role with permissions
+router.post("/roles/create", adminAuthMiddleware, async (req, res) => {
+  try {
+    const { name, description, permissions } = req.body;
+
+    if (!name) {
+      return res
+        .status(400)
+        .send(HelperUtils.errorObj("Role name is required"));
+    }
+    if (!description) {
+      return res
+        .status(400)
+        .send(HelperUtils.errorObj("Description is required"));
+    }
+    if (!Array.isArray(permissions) || permissions.length === 0) {
+      return res
+        .status(400)
+        .send(HelperUtils.errorObj("Permissions must be a non-empty array"));
+    }
+    const Userrole = req.user.roles;
+    if (!Userrole.includes("SuperAdmin")) {
+      return res
+        .status(401)
+        .send(
+          HelperUtils.errorObj(
+            "Access denied. only super admin can create this data."
+          )
+        );
+    }
+
+    const role = await Roles.create({ name, description });
+
+    // Create the entry in the RolePermissions table
+    const rolePermissionsData = permissions.map((permissionId) => ({
+      roleId: role.id,
+      permissionId,
+    }));
+    console.log({ rolePermissionsData });
+    const createdPermissions = await RolePermissions.bulkCreate(
+      rolePermissionsData
+    );
+    if (!createdPermissions) {
+      return res
+        .status(500)
+        .send(HelperUtils.errorObj("Failed to create role permissions"));
+    }
+
+    res
+      .status(201)
+      .send(HelperUtils.successObj("Role created successfully", role));
+  } catch (err) {
+    console.error("Error creating role:", err);
+    res.status(500).send(HelperUtils.errorObj("Failed to create role"));
+  }
+});
+
+// Get All Employers
+router.get("/employers", adminAuthMiddleware, async (req, res) => {
+  try {
+    const employers = await Users.findAll({
+      where: {
+        employerCode: {
+          [Op.ne]: null, // fetch where employerCode is NOT NULL
+        },
+      },
+    });
+
+    res
+      .status(200)
+      .send(
+        HelperUtils.successObj("Employers fetched successfully", employers)
+      );
+  } catch (err) {
+    console.error("Error fetching employers:", err);
+    res.status(500).send(HelperUtils.errorObj("Unable to fetch employers"));
   }
 });
 
--- startup/database.js
+++ startup/database.js
@@ -1,30 +1,30 @@
-const { Sequelize } = require('sequelize');
-const config = require('config');
+const { Sequelize } = require("sequelize");
+const config = require("config");
 // MySQL config
-const DB_NAME = config.get('DB_NAME');
-const sequelize = new Sequelize(DB_NAME, 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+const DB_NAME = config.get("DB_NAME");
+const sequelize = new Sequelize(DB_NAME, "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
   logging: console.log, // Set to false to disable SQL logs
   define: {
     freezeTableName: true,
-    timestamps: true // Adds createdAt, updatedAt fields
+    timestamps: true, // Adds createdAt, updatedAt fields
   },
   pool: {
     max: 10,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
 
 // Connection check
 async function connectToDatabase() {
   try {
     await sequelize.authenticate();
-    console.log('✅ Connected to MySQL (work_win) successfully.');
+    console.log("✅ Connected to MySQL (work_win) successfully.");
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
+    console.error("❌ Unable to connect to the database:", error);
   }
 }
 
